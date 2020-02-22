556db2d9bf2f5ad8f2eae377ed2da9f61528da5e
Aimin <aimin987@163.com>
2020-02-22 11:01:00 +0800

    图片练习

--- lib/examples/image.dart
+++ lib/examples/image.dart
@@ -4,7 +4,7 @@
  * Description: 
  * -----
  * Created  By: Aim 2020-02-21 17:42:05
- * Modified By: Aim 2020-02-21 18:03:10
+ * Modified By: Aim 2020-02-22 10:57:42
  * -----
  * HISTORY:
  * Date      	By	Comments
@@ -27,8 +27,34 @@ class ImageWidget extends StatelessWidget {
           width: 64,
         ),
         Image.network(
-          'https://avatars2.githubusercontent.com/u/20411648?s=460&v=4',
+          'https://cdn.jsdelivr.net/gh/flutterchina/website@1.0/images/flutter-mark-square-100.png',
           width: 100,
+        ),
+        Image(
+          image: NetworkImage(
+              'https://cdn.jsdelivr.net/gh/flutterchina/website@1.0/images/flutter-mark-square-100.png'),
+        ),
+        Text(
+          '\uE914\uE000\uE90D\uE914\uE000\uE90D',
+          style: TextStyle(
+              fontFamily: 'MaterialIcons', fontSize: 24, color: Colors.green),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Icon(
+              Icons.accessible,
+              color: Colors.green,
+            ),
+            Icon(
+              Icons.error,
+              color: Colors.green,
+            ),
+            Icon(
+              Icons.fingerprint,
+              color: Colors.green,
+            ),
+          ],
         )
       ],
     );
