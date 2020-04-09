42f3447c7ef761e20b845192794e5d2c5135934a
Aimin <aimin987@163.com>
2020-04-09 20:05:13 +0800

    7.3 跨组件状态共享（Provider）

--- lib/chapter7/chapter7.dart
+++ lib/chapter7/chapter7.dart
@@ -4,7 +4,7 @@
  * Description: 第7章 功能型组件
  * -----
  * Created  By: Aim 2020-04-08 17:44:24
- * Modified By: Aim 2020-04-09 13:42:38
+ * Modified By: Aim 2020-04-09 16:51:59
  * -----
  * HISTORY:
  * Date      	By	Comments
@@ -14,6 +14,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_tutorial/chapter7/func_inherited.dart';
+import 'package:flutter_tutorial/chapter7/func_provider.dart';
 import 'package:flutter_tutorial/chapter7/func_willpopscope.dart';
 
 class Chapter7 extends StatelessWidget {
@@ -43,6 +44,15 @@ class Chapter7 extends StatelessWidget {
               onPressed: () {
                 Navigator.push(context, MaterialPageRoute(builder: (context) {
                   return FuncInherited();
+                }));
+              },
+            ),
+
+            FlatButton(
+              child: Text("7.3 跨组件状态共享（Provider）"),
+              onPressed: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) {
+                  return FuncProvider();
                 }));
               },
             ),
---
+++ lib/chapter7/func_provider.dart
@@ -0,0 +1,169 @@
+/*
+ * File: func_provider.dart
+ * -----
+ * Description: 7.3 跨组件状态共享（Provider）
+ * -----
+ * Created  By: Aim 2020-04-09 14:49:51
+ * Modified By: Aim 2020-04-09 16:50:36
+ * -----
+ * HISTORY:
+ * Date      	By	Comments
+ * ----------	---	----------------------------------------------------------
+ */
+
+import 'dart:collection';
+
+import 'package:flutter/material.dart';
+
+///
+class InheritedProvider<T> extends InheritedWidget {
+  InheritedProvider({@required this.data, Widget child}) : super(child: child);
+
+  final T data;
+
+  @override
+  bool updateShouldNotify(InheritedProvider<T> oldWidget) {
+    //在此简单返回true，则每次更新都会调用依赖其的子孙节点的`didChangeDependencies`。
+    return true;
+  }
+}
+
+// 该方法用于在Dart中获取模板类型
+// Type _typeOf<T>() => T;
+
+///
+class ChangeNotifier implements Listenable {
+  @override
+  void addListener(listener) {
+    // TODO: implement addListener
+  }
+
+  @override
+  void removeListener(listener) {
+    // TODO: implement removeListener
+  }
+
+  void notifyListeners() {
+    //通知所有监听器，触发监听器回调
+  }
+}
+
+///
+
+class ChangeNotifierProvider<T extends ChangeNotifier> extends StatefulWidget {
+  ChangeNotifierProvider({Key key, this.data, this.child});
+
+  final Widget child;
+  final T data;
+
+  static T of<T>(BuildContext context) {
+    // final type = _typeOf<InheritedProvider<T>>();
+    final provider =
+        context.dependOnInheritedWidgetOfExactType<InheritedProvider<T>>();
+    return provider.data;
+  }
+
+  @override
+  _ChangeNotifierProviderState<T> createState() =>
+      _ChangeNotifierProviderState<T>();
+}
+
+class _ChangeNotifierProviderState<T extends ChangeNotifier>
+    extends State<ChangeNotifierProvider<T>> {
+  @override
+  void initState() {
+    widget.data.addListener(update);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    widget.data.removeListener(update);
+    super.dispose();
+  }
+
+  void update() {
+    //如果数据发生变化（model类调用了notifyListeners），重新构建InheritedProvider
+    setState(() => {});
+  }
+
+  @override
+  void didUpdateWidget(ChangeNotifierProvider<T> oldWidget) {
+    if (widget.data != oldWidget.data) {
+      oldWidget.data.removeListener(update);
+      widget.data.addListener(update);
+    }
+    super.didUpdateWidget(oldWidget);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return InheritedProvider<T>(
+      data: widget.data,
+      child: widget.child,
+    );
+  }
+}
+
+class Item {
+  Item(this.price, this.count);
+  double price;
+  int count;
+}
+
+class CartModel extends ChangeNotifier {
+  final List<Item> _items = [];
+
+  UnmodifiableListView<Item> get items => UnmodifiableListView(_items);
+
+  double get totalPrice =>
+      _items.fold(0, (value, item) => value + item.count * item.price);
+
+  void add(Item item) {
+    _items.add(item);
+
+    notifyListeners();
+  }
+}
+
+class FuncProvider extends StatefulWidget {
+  @override
+  _FuncProviderState createState() => _FuncProviderState();
+}
+
+class _FuncProviderState extends State<FuncProvider> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('7.3 跨组件状态共享（Provider）'),
+      ),
+      body: Center(
+        child: ChangeNotifierProvider<CartModel>(
+          data: CartModel(),
+          child: Builder(builder: (context) {
+            return Column(
+              children: <Widget>[
+                Builder(builder: (context) {
+                  var cart = ChangeNotifierProvider.of<CartModel>(context);
+                  return Text("总价: ${cart.totalPrice}");
+                }),
+                Builder(builder: (context) {
+                  print("RaisedButton build"); //在后面优化部分会用到
+                  return RaisedButton(
+                    child: Text("添加商品"),
+                    onPressed: () {
+                      //给购物车中添加商品，添加后总价会更新
+                      ChangeNotifierProvider.of<CartModel>(context)
+                          .add(Item(20.0, 1));
+                    },
+                  );
+                }),
+              ],
+            );
+          }),
+        ),
+      ),
+    );
+  }
+}
